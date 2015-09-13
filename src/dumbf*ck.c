402705e208e3a799a25f350eb3e3cf24ed15a66a
eyob-- <tse.eyob@gmail.com>
2015-09-13 04:44:23 -0400

    Revert "fixed" syntax

--- src/dumbf*ck.c
+++ src/dumbf*ck.c
@@ -5,13 +5,14 @@ char s[5000], *p = s;
 g(char c) {
 	switch (c) {
 		case '<':
-			p--; break;
-		case '>':
 			p++; break;
-		case '!':
-			*p ^= 1 << 0; break;
+		case '>':
+			p--; break;
 		case '.':
+			*p ^= 1 << 0; break;
+		case ',':
 			printf("%c",*p); break;
+
 	}
 }
 
