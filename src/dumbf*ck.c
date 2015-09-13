cfa2cca086a118938ac32d2dc90aeba99b8832d7
eyob-- <tse.eyob@gmail.com>
2015-09-13 18:39:51 -0400

    Fix read and add branch op

--- src/dumbf*ck.c
+++ src/dumbf*ck.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 unsigned char s[5000], *p = s;
+long q = 0;
+int x = 0;
 
 void printbinchar(char c)
 {
@@ -10,7 +12,11 @@ void printbinchar(char c)
     }
 }
 
-g(char c) {
+g(char c, FILE *f) {
+	char u;
+	if (x){if (c == '?') x = 0; return 0;}
+	//printf("\n%d\n",q);
+
 	switch (c) {
 		case '<':
 			p++; break;
@@ -21,9 +27,11 @@ g(char c) {
 		case ',':
 			printf("%c",*p); break;
 		case '\'':
-			scanf("%c",p); break;
+			scanf("%c",p);scanf("%c",&u); break;
 		case '/':
 			*p = (*p >> 3) | (*p << 5); break;
+		case '?':
+			if (*p % 2 == 0){ q = ftell(f); x = 1;} else {fseek(f, q+1, SEEK_SET);} break;
 	}
 	/*printbinchar(*p);
 	printf(" ");
@@ -36,5 +44,6 @@ g(char c) {
 main(int a, char** b) {
 	char c;
 	FILE *f = fopen(b[1], "r");
-	while (fscanf(f, "%c", &c) != EOF) g(c);
+	while (fscanf(f, "%c", &c) != EOF) g(c,f);
+	fclose(f);
 }
